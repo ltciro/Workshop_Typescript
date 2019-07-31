7d96b26a3e1656738e106a6271ed9eb604f903da
Laura Ciro <ltciro@gmail.com>
2019-07-31 14:56:34 -0500

    Update app.routing.js

--- src/app.routing.js
+++ src/app.routing.js
@@ -3,9 +3,7 @@ const postsController =  require('./api/posts.controller');
 const InMemoryPostsRepository = require ('./data_access/in-memory-posts.repository');
 const PostsService = require('./services/posts.service');
 
-console.log(InMemoryPostsRepository, PostsService, postsController)
 let postsService = new PostsService(new InMemoryPostsRepository());
-console.log(postsService)
 function setRouting(app) {
     app.use('/api/posts', postsController(postsService));
 }
